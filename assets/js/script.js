7f5de3c5ac0d2ef090d055ef7409a08d726656aa
twitch10126 <twitch10126@hotmail.com>
2025-04-28 15:02:47 +0100

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function runGame (){}
+
+function checkAnswer(){}
+
+function calculateCorrectAnswer(){}
+
+function incrementScore(){}
+
+function incrementWrongAnswer(){}
+
+function displayAdditionQuestion(){}
+
+function displaySubtractQuestion(){}
+
+function displayMultiplyQuestion(){}
