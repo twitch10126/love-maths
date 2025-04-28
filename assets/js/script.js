f969968a07d58a0f3dea293a7185cab51e65d801
twitch10126 <twitch10126@hotmail.com>
2025-04-28 16:04:34 +0100

    Display addition question tot he user

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,17 +20,27 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
     }
+
+runGame("addition"); 
+
 })
 
 /** 
  * The main game "loop*, called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(){
+function runGame(gameType){
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1,num2);
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unkown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer(){}
@@ -41,7 +51,11 @@ function incrementScore(){}
 
 function incrementWrongAnswer(){}
 
-function displayAdditionQuestion(){}
+function displayAdditionQuestion(operand1,operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
+}
 
 function displaySubtractQuestion(){}
 
